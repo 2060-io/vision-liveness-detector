0a3db4e15a766793bae554d5e1c647f6777f1422
Ariel Gentile <gentilester@gmail.com>
2025-04-24 20:05:20 -0300

    build: files for 0.1.0 manual publishing
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- setup_for_build.sh
+++ setup_for_build.sh
@@ -4,12 +4,19 @@
 set -e
 
 # Define the source and destination directories
-SOURCE1="livenessDetector"
-SOURCE2="livenessDetectorServerApp"
-TARGET="mediapipe/"
+SOURCE1="src/livenessDetector"
+SOURCE2="src/livenessDetectorServerApp"
+TARGET_PATH="build/mediapipe/"
+MEDIAPIPE_URL="https://github.com/google-ai-edge/mediapipe"
+MEDIAPIPE_TAG="v0.10.20"
+
+# Clone Mediapipe, which will be used as a base for compilation
+if [ ! -d "$TARGET_PATH" ] ; then
+    git clone --depth 1 --branch $MEDIAPIPE_TAG $MEDIAPIPE_URL $TARGET_PATH
+fi
 
 # Copy the source directories into the target location within MediaPipe
-cp -r "$SOURCE1" "$TARGET"
-cp -r "$SOURCE2" "$TARGET"
+cp -r "$SOURCE1" "$TARGET_PATH"
+cp -r "$SOURCE2" "$TARGET_PATH"
 
-echo "Successfully copied $SOURCE1 and $SOURCE2 into $TARGET."
+echo "Successfully copied $SOURCE1 and $SOURCE2 into $TARGET_PATH."
