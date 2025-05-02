cb198844cf12177ba89ea496efb8ba07876d5cf6
Diego Aguilar <diego.alfonso.aguilar.cardona@gmail.com>
2025-05-02 17:47:09 -0500

    moved wheel generation to wrappers

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -e
 
 DOCKER_BUILDKIT=1 docker build -f Dockerfile.manylinux_2_28_x86_64 -t mediapipe_liveness_detector:latest --build-arg PYTHON_BIN=/opt/python/cp312-cp312/bin/python3.12 .
 docker create -ti --name liveness_detector_pip_package_container mediapipe_liveness_detector:latest
-docker cp liveness_detector_pip_package_container:/livenessDetector/wrappers/python/dist dist/
+docker cp liveness_detector_pip_package_container:/livenessDetector/wrappers/python/dist wrappers/python/dist/
 docker rm -f liveness_detector_pip_package_container
