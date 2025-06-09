400fe89712f115fcda4daa5fe88840a7feee0ba5
Ariel Gentile <gentilester@gmail.com>
2025-06-09 20:25:27 -0300

    ci: add build image workflow
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 
 # cleanup previous builds
 rm -f wrappers/python/dist/*
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.manylinux_2_28_x86_64 -t mediapipe_liveness_detector:latest --build-arg PYTHON_BIN=/opt/python/cp312-cp312/bin/python3.12 .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.manylinux_2_28_x86_64 -t mediapipe_liveness_detector:latest --build-arg PYTHON_BIN=/opt/python/cp312-cp312/bin/python3.12 --build-arg RELEASE_VERSION=$RELEASE_VERSION .
 docker create -ti --name liveness_detector_pip_package_container mediapipe_liveness_detector:latest
 docker cp liveness_detector_pip_package_container:/livenessDetector/wrappers/python/dist wrappers/python
 docker rm -f liveness_detector_pip_package_container
